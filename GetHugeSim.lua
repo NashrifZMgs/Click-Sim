baa4a9dd38ef6f9e89b20cf60e665eab694262b7
NashrifZMgs <84576211+NashrifZMgs@users.noreply.github.com>
2024-01-05 19:03:28 +0800

    a

--- GetHugeSim.lua
+++ GetHugeSim.lua
@@ -5,11 +5,9 @@ getgenv().sellStrength = false;
 --main--
 function AutoLiftWeight()
     spawn(function()
-        if getgenv().liftWeight == true then
-            while getgenv().liftWeight == true do
-                game:GetService("ReplicatedStorage").Remotes.LiftWeight:FireServer()
-                wait()
-            end
+        while getgenv().liftWeight == true do
+            game:GetService("ReplicatedStorage").Remotes.LiftWeight:FireServer()
+            wait()
         end
     end)
 end
