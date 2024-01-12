1e584b24ab8fab5a1a3df5373119f18c6d5699f4
NashrifZMgs <84576211+NashrifZMgs@users.noreply.github.com>
2024-01-12 18:43:09 +0800

    RebirthSim.lua

---
+++ RebirthSim.lua
@@ -0,0 +1,16 @@
+getgenv().click = true;
+
+
+
+
+
+
+
+function AutoClick()
+    spawn(function()
+        while getgenv().click == true do
+            game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("Click3"):FireServer()
+            wait()
+        end
+    end)
+end
