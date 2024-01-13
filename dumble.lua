9a0e753b9a1bc2e70f110af56ac383dcf6d66fae
NashrifZMgs <84576211+NashrifZMgs@users.noreply.github.com>
2024-01-13 13:39:32 +0800

    update

--- dumble.lua
+++ dumble.lua
@@ -21,7 +21,7 @@ function AutoDumble(dumbles)
     spawn(function()
         while getgenv().dumble == true do
             local args = {
-                [1] = game:GetService("Players").LocalPlayer.Character:FindFirstChild(dumbles)
+                [1] = game:GetService("Players").LocalPlayer.Character:FindFirstChild(dumbles[8])
             }
             game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("Event"):WaitForChild("Game"):WaitForChild("[C-S]PlayerClick"):FireServer(unpack(args))
             wait()
