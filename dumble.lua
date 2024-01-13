f96cd32097e849b110dfba7d4b8b714e3a45d180
NashrifZMgs <84576211+NashrifZMgs@users.noreply.github.com>
2024-01-13 13:18:01 +0800

    update

--- dumble.lua
+++ dumble.lua
@@ -1,14 +1,14 @@
-getgenv().dumble = true;
+getgenv().win = true;
 
-local dumbles = {"1_1","1_2","1_3","1_4","1_5"}
 
-function AutoDumble(dumbles)
+function Autowin()
     spawn(function()
-    while getgenv().dumble == true do
+    while getgenv().win == true do
         local args = {
-            [1] = game:GetService("Players").LocalPlayer.Character:FindFirstChild(dumbles)
+            [1] = true,
+            [2] = 1
         }
-        game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("Event"):WaitForChild("Game"):WaitForChild("[C-S]PlayerClick"):FireServer(unpack(args))
+        game:GetService("ReplicatedStorage"):WaitForChild("Remote"):WaitForChild("Event"):WaitForChild("Game"):WaitForChild("[C-S]PlayerEnd"):FireServer(unpack(args))
     end)
 end
-AutoDumble(dumbles)
+AutoWin()
